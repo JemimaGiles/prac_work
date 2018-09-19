a7da8deec8bb6e3eec47d16b2eb51e3d01c0e39a
JemimaGiles <jemima.giles@my.jcu.edu.au>
2018-09-19 10:20:17 +1000

    Added conversion pracs - still in need of fixing

---
+++ prac_07/converting_distances.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KM = 1.60934
+
+
+class ConvertDistancesApp(App):
+
+    def build(self):
+        self.title = 'Convert miles to kilometers'
+        self.root = Builder.load_file('converting_distances.kv')
+        return self.root
+
+    def error_check(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_conversion(self):
+        value = self.error_check()
+        result = value * MILES_TO_KM
+        self.root.ids.output_lable.text = str(result)
+
+    def handle_increment(self, change):
+        value = self.error_check() + change
+        self.root.ids.input_number.text = str(value)
+        self.handle_conversion()
+
+
+ConvertDistancesApp().run()
