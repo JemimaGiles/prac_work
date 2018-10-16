9e0ea7a645d2903881954165751b7bef45db8eab
JemimaGiles <jemima.giles@my.jcu.edu.au>
2018-10-16 13:20:35 +1000

    Edits silver_service_taxi and Taxi class

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -2,15 +2,15 @@ from prac_08.SilverServiceTaxi import SilverServiceTaxi
 
 
 def main():
-    astin_martin = SilverServiceTaxi(100, '007', 5)
+    aston_martin = SilverServiceTaxi(100, '007', 5)
     porsche = SilverServiceTaxi(100, "Porscha", 3)
     honda = SilverServiceTaxi(100, "Lucy", 2)
 
-    astin_martin.drive(30)
+    aston_martin.drive(30)
     porsche.drive(20)
     honda.drive(18)
 
-    print(astin_martin)
+    print(aston_martin)
     print(porsche)
     print(honda)
 
